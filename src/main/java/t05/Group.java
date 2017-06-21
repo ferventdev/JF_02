e70459df313f0c26f28bdbea64315de6075f3af3
ferventdev <alex.4everyours@gmail.com>
2017-06-21 13:31:26 +0300

    Task 5 - class Student created.

---
+++ src/main/java/t05/Group.java
@@ -0,0 +1,7 @@
+package t05;
+
+/**
+ * Created by Aleksandr Shevkunenko on 21.06.2017.
+ */
+public enum Group {
+}
---
+++ src/test/java/t05/StudentTest.java
@@ -0,0 +1,15 @@
+package t05;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Aleksandr Shevkunenko on 21.06.2017.
+ */
+public class StudentTest {
+    @Test
+    public void studentTest() throws Exception {
+
+    }
+}
